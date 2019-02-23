3ab753f2efd173fb1cb3f67c99a896412ce3e355
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-23 06:40:00 +0000

    adds user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      @session[:user_id] = @user.id
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -24,4 +24,5 @@ class UsersController < ApplicationController
       :password_confirmation
     )
   end
+
 end
