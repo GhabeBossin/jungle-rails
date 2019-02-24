73af097926ad9fe266e25a15a9c78b54b425bbbc
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-24 05:19:38 +0000

    fixes typo in db

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20190224050230) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "product_id"
-    t.integer  "uder_id"
+    t.integer  "user_id"
     t.text     "description"
     t.integer  "rating"
     t.datetime "created_at",  null: false
