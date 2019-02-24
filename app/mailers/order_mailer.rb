a92cc2d657e182f5b506d89b49e91c215a45cc90
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-24 03:36:07 +0000

    emails reciept to user

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,13 @@
 class OrderMailer < ApplicationMailer
+
+  default from: 'no-reply@jungle.com'
+  layout 'mailer'
+
+  def order_email(order)
+    @order = order
+    @email = order.email
+    mail(to: @email, subject: "Your Jungle.com Order: #{@order.id}")
+  end
+
 end
+
