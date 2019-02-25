3a199f305e5d109a1ea52a2d51c76afc8f9800b3
Ghabe Bossin <gee.bossin@gmail.com>
2019-02-25 17:57:59 +0000

    review delete button to right, align sign-in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,33 @@
 class ReviewsController < ApplicationController
+
   def create
-    raise 'woohoo yaaaas'
+    @product = Product.find params[:product_id]
+    @review = Review.create(review_params)
+    @review.user = current_user
+    @review.product_id = @product.id
+
+    if @review.save!
+      redirect_to product_path(@product.id)
+    else
+      render product_path(@product.id)
+    end
   end
+
+  def destroy
+    @review = Review.find params[:review_id]
+    @review.destroy
+    redirect_to [:product], notice: 'Review deleted!'
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+      :product_id,
+      :id
+    )
+  end
+
 end
